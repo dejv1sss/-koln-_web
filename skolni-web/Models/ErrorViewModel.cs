ae8c82df9129b4ed52c05736b7c0db63795f3e30
David Otradovec <otradovecdavid53@gmail.com>
2026-05-27 10:19:46 +0200

    zalozeni repozitáře

---
+++ skolni-web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace skolni_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
